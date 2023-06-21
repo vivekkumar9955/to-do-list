c8e916c07e9015fd1984a02e730ac026bf9d1cf9
vivekkumar9955 <126938288+vivekkumar9955@users.noreply.github.com>
2023-06-21 12:27:38 +0530

    to do list project

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            list-style: none;
+            text-decoration: none;
+            background: rgb(2, 0, 36);
+            background: linear-gradient(90deg, rgba(2, 0, 36, 0) 0%, rgba(235, 0, 255, 0.5020133053221288) 100%);
+        }
+        h1{
+           text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>Grocery List</h1>
+</body>
+
+</html>
